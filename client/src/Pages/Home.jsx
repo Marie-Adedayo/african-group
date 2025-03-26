08665508b0be1bcd26fd0c2047da1f651d22594f
dedoyin21 <adedirandedoyin@gmail.com>
2025-03-26 11:33:13 +0100

    African group

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+'use client'
+import ServicesLayout from '../Components/Services'
+
+
+const Home = () => {
+
+  return (
+    <main className="relative">
+      <div className="w-full">
+        <ServicesLayout />
+      </div>
+    </main>
+  )
+}
+
+export default Home
