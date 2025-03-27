61b458f5b664ea4153957c4ac7cd498c97b0307d
Marie-Adedayo <fasanyaadedayo14@gmail.com>
2025-03-27 09:12:24 +0100

    Navbar

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,15 @@
 'use client'
 import ServicesLayout from '../Components/Services'
+import Navbar from '../Components/Navbar'
 
 
 const Home = () => {
 
   return (
     <main className="relative">
+      <Navbar />
       <div className="w-full">
+      
         <ServicesLayout />
       </div>
     </main>
