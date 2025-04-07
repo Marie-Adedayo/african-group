0d30874c3f0d826cec725eb2ac05f9bf3b5f5cff
Marie-Adedayo <fasanyaadedayo14@gmail.com>
2025-04-07 13:34:02 +0100

    Correction

--- client/src/Components/SurveyHero.jsx
+++ client/src/Components/SurveyHero.jsx
@@ -6,7 +6,7 @@ const SurveyHero = () => {
         <>
             <div className="bg-[#071a0a] text-[#FCFFFC]">
                 {/* Breadcrumb Section */}
-                <ul className="flex items-center gap-[0.6875rem] md:pt-[7rem] md:pb-4 pl-4 border-t-[1px] border-b-[1px] border-[#FCFFFC] text-[10px] leading-none uppercase">
+                <ul className="flex items-center gap-[0.6875rem] pt-[3.5rem] md:pt-[7rem] pb-1 md:pb-4 pl-4 border-t-[1px] border-b-[1px] border-[#FCFFFC] text-[10px] leading-none uppercase">
                     <li>
                     <a href="/" className="">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 10" className="w-3 h-3">
@@ -30,40 +30,42 @@ const SurveyHero = () => {
                     <li className="">Survey</li>
                 </ul>
                 {/* Hero Section */}
-                <div
-                    className="relative h-[90vh] overflow-hidden rounded-[0.26438rem] bg-[#FCFFFC] flex flex-col justify-between p-[1.8125rem_1.125rem] text-[#071a0a]"
-                   
-                >
-                    {/* Gradient Overlay */}
-                    <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.2)] z-[-1]"></div>
-                        {/* Content */}
-                       <div className="relative z-1 flex flex-col gap-4 p-6">
-                          <p className="text-sm tracking-wide uppercase font-medium">Property Search</p>
-                          <h1 className="text-2xl md:text-4xl font-bold leading-[1.2] w-[400px]">
-                              End-to-end and box-to-box.
-                          </h1>
-                          
+                <div className="bg-[#071a0a] px-4 py-7 md:px-10 md:py-12">
+                    
+                    <div
+                        className="relative h-[85vh]  md:h-[68vh] overflow-hidden rounded-[0.26438rem] bg-[#FCFFFC] flex flex-col justify-between p-[1.8125rem_1.125rem] text-[#071a0a]"
+                    
+                    >
+                        {/* Gradient Overlay */}
+                        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.2)] z-[-1]"></div>
+                            {/* Content */}
+                        <div className="relative z-1 flex flex-col gap-4 p-6">
+                            <div className=" opacity-100 flex flex-row bg-gray-300 w-[12rem] px-2 py-1 gap-2 text-sm tracking-wide uppercase font-medium">
+                                <span><div className="w-2 h-2 mt-2 bg-[#248232]"></div></span>
+                                Property Search
+                            </div>
+                            <h1 className="text-2xl md:text-4xl font-bold leading-[1.2] w-[400px]">
+                                End-to-end and box-to-box.
+                            </h1>
+                            
 
+                            </div>
+                                                
+                        <div className="w-[300px]  group">
+                            <a
+                                className="flex item-end justify-between group text-lg"
+                                href="/about#esg"
+                                target="_self"
+                            >
+                                <span className=" "> Key Contact</span>
+                                <FiArrowUpRight className="w-5 h-5 md:w-6 md:h-6 text-gray-500 group-hover:text-[#071a0a] item-end justify-end transition-transform duration-500 group-hover:rotate-45 group-hover:scale-110" />
+                            </a>
+                            {/* Line under the text */}
+                            <div className="w-full h-[1px] mt-2 bg-gray-500 group-hover:bg-[#071a0a] transition-all"></div>
                         </div>
-                                            
-                    <div className="w-full md:w-[400px] group">
-                        <a
-                            className="flex item-end justify-between group text-lg"
-                            href="/about#esg"
-                            target="_self"
-                        >
-                            <span className=" "> Key Contact</span>
-                            <FiArrowUpRight className="w-5 h-5 md:w-6 md:h-6 text-gray-500 group-hover:text-[#FCFFFC] item-end justify-end transition-transform duration-500 group-hover:rotate-45 group-hover:scale-110" />
-                        </a>
-                        {/* Line under the text */}
-                        <div className="w-full h-[1px] mt-2 bg-gray-500 group-hover:bg-[#FCFFFC] transition-all"></div>
+                    
                     </div>
-                
                 </div>
-
-
-
-
             </div>
         </>
 
